929b164092e87025fab8ffcd7ba7067e3c9762d5
hamadanaoto <teiru_3@yahoo.co.jp>
2015-07-08 18:23:26 +0900

    rescure.rb, variable.rb

---
+++ src/rescue.rb
@@ -0,0 +1,8 @@
+# encoding:utf-8
+
+begin
+  1 / 0
+rescue ZeroDivisionError
+  puts 'ZeroDivisionError!!'
+end
+
---
+++ src/variable.rb
@@ -0,0 +1,15 @@
+# encoding:utf-8
+
+puts true
+puts false
+puts nil
+puts self
+puts __FILE__
+puts __LINE__
+puts __ENCODING__
+
+puts $$
+puts $?
+puts $DEBUG
+
+
